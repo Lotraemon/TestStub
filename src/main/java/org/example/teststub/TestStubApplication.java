d703c3d843f8522f0b38ef04c711430d294706c5
Lotraemon <Lotraemon@gmail.com>
2025-05-11 13:04:10 +0300

    initial commit

---
+++ src/main/java/org/example/teststub/TestStubApplication.java
@@ -0,0 +1,13 @@
+package org.example.teststub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestStubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestStubApplication.class, args);
+    }
+
+}
